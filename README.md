875277b531151fd7f77ab4f37a4cc43ca64e5558
FengJianLL <851269108@qq.com>
2017-09-21 17:34:35 +0800

    修改README文件，增加class.rb文件

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # My-first-project
 This the first time I use git!
+Today is Sep.21 
+I am learning git.
---
+++ class.rb
@@ -0,0 +1,7 @@
+class << helloWorld
+	def hi(name)
+		puts "#{name} said hi!"
+	end
+end
+
+helloWorld.hi("Tom")
